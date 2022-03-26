f177545c01f94f1e887cf143053450a2f41bef9a
shorewind <99942150+shorewind@users.noreply.github.com>
2022-03-26 14:30:39 -0500

    Update nTPTB.py

--- nTPTB.py
+++ nTPTB.py
@@ -1,11 +1,9 @@
-from lib2to3.pgen2.tokenize import tokenize
-import discord
 from discord.ext import commands
-import nltk 
 import json
 import os
-import translators as ts
-
+import nltk
+from lib2to3.pgen2.tokenize import tokenize
+import translators as translate
 
 if os.path.exists(os.getcwd() + "/config.json"):
     with open(".\config.json") as f:
@@ -15,31 +13,44 @@ else:
     with open(os.getcwd() + "/config.json","w+") as f:
         json.dump(configTemplate,f)
 
-
 bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
 
 
 class translator(commands.Cog):
     def __init__(self,bot): 
         self.bot = bot
- 
+
     @commands.command(name= "test")
     async def test(self,ctx,*,message):
         puncts = nltk.tokenize.wordpunct_tokenize(message)  
         return await ctx.send(puncts)
 
+    @commands.command(name='terminate')
+    async def terminate(self, ctx):
+        await ctx.send('Goodbye')
+        await bot.logout()
+
+    @commands.command(name='hello')
+    async def test(self,ctx):
+        return await ctx.send("Hello, world!")
+
+    @commands.command(name='echo')
+    async def echo(self, ctx, *, message):
+        return await ctx.send(message)
+   
     @commands.command(name='translate', alias = 'tl')
     async def translate(self,ctx,*,message):
         puncts = nltk.tokenize.wordpunct_tokenize(message)  
         eng = []
         outString = ""
-        eng.append(ts.google(str(puncts)))
+        eng.append(translate.google(str(puncts)))
         for word in eng:
             outString +=  " " + word
         return await ctx.send(str(outString))
-            
-
-
 
 token  = configData["token"]
 def setup(bot):
