cb958a1130e558e490091c94fa4d015414e819ba
shorewind <99942150+shorewind@users.noreply.github.com>
2022-03-26 17:29:53 -0500

    added country guide and help message

--- nTPTB.py
+++ nTPTB.py
@@ -1,12 +1,8 @@
 from discord.ext import commands
 import json
 import os
-import nltk
 from lib2to3.pgen2.tokenize import tokenize
 import translators as translate
-import re
-
-
 
 if os.path.exists(os.getcwd() + "/config.json"):
     with open(".\config.json") as f:
@@ -16,8 +12,7 @@ else:
     with open(os.getcwd() + "/config.json","w+") as f:
         json.dump(configTemplate,f)
 
-
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='$', help_command=None)
 
 @bot.event
 async def on_ready():
@@ -27,11 +22,6 @@ async def on_ready():
 class translator(commands.Cog):
     def __init__(self,bot): 
         self.bot = bot
-
-    @commands.command(name= "test")
-    async def test(self,ctx,*,message):
-        puncts = nltk.tokenize.wordpunct_tokenize(message)  
-        return await ctx.send(puncts)
 
     @commands.command(name='hello')
     async def test(self,ctx):
@@ -47,15 +37,23 @@ class translator(commands.Cog):
         return await ctx.send(translated_message)
 
     @commands.command(name = 'TranslateTTs', aliases = ['tltts'])
-    async def translateTTS(self,ctx, *, message):
-         translated_message =(translate.google(message))
+    async def translateTTS(self,ctx, from_language, to_language, *, message):
+         translated_message = translate.google(message, str(from_language), str(to_language))
          return await ctx.send(translated_message, tts=True)
 
     @commands.command(name = 'countryGuide', aliases = ['cg', 'cG', 'Cg'])
     async def countryGuide(self, ctx):
-        return await ctx.send("")
-    
-    
+        file = open("countries.txt", 'r')
+        await ctx.send(file.read())
+        file.close()
+
+    @commands.command(name = 'help', aliases = ['h', '?'])
+    async def helper(self, ctx, to_language='en'):
+        help_message = 'help message'
+        translated_help_message = translate.google(help_message, 'en', str(to_language))
+        return await ctx.send(translated_help_message)
+
+
 token  = configData["token"]
 def setup(bot):
     bot.add_cog(translator(bot))
