4e5bbc1df8617b65b1149676cc39857b82d91502
nlive65 <94426110+nlive65@users.noreply.github.com>
2022-03-25 22:33:29 -0500

    Add files via upload

---
+++ nTPTB.py
@@ -0,0 +1,8 @@
+import discord
+from discord.ext import commands
+import nltk 
+
+client = discord.Client()
+bot = commands.Bot(command_prefix='$')
+
+bot.run('OTU3MDkzNTQ3ODM4OTM5MTM2.Yj5w0w.CvtXi2-UfFc8cZ6ckY7Ywd86CdY')
