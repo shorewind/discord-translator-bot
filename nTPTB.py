081d5e47660563ae14f56930063bbacce8d7fafc
nlive65 <94426110+nlive65@users.noreply.github.com>
2022-03-26 14:17:41 -0500

    Add files via upload

--- nTPTB.py
+++ nTPTB.py
@@ -1,7 +1,11 @@
+from lib2to3.pgen2.tokenize import tokenize
+import discord
 from discord.ext import commands
+import nltk 
 import json
 import os
-import translators as translate
+import translators as ts
+
 
 if os.path.exists(os.getcwd() + "/config.json"):
     with open(".\config.json") as f:
@@ -11,34 +15,30 @@ else:
     with open(os.getcwd() + "/config.json","w+") as f:
         json.dump(configTemplate,f)
 
-bot = commands.Bot(command_prefix='$')
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+bot = commands.Bot(command_prefix='$')
 
 
 class translator(commands.Cog):
     def __init__(self,bot): 
         self.bot = bot
+ 
+    @commands.command(name= "test")
+    async def test(self,ctx,*,message):
+        puncts = nltk.tokenize.wordpunct_tokenize(message)  
+        return await ctx.send(puncts)
 
-    @commands.command(name='terminate')
-    async def terminate(self, ctx):
-        await ctx.send('Goodbye')
-        await bot.logout()
+    @commands.command(name='translate', alias = 'tl')
+    async def translate(self,ctx,*,message):
+        puncts = nltk.tokenize.wordpunct_tokenize(message)  
+        eng = []
+        outString = ""
+        eng.append(ts.google(str(puncts)))
+        for word in eng:
+            outString +=  " " + word
+        return await ctx.send(str(outString))
+            
 
-    @commands.command(name='hello')
-    async def test(self,ctx):
-        return await ctx.send("Hello, world!")
-
-    @commands.command(name='echo')
-    async def echo(self, ctx, *, message):
-        return await ctx.send(message)
-   
-    @commands.command(name='translate')
-    async def translate(self, ctx, *, message):
-        text_translated = translate.google(message)
-        await ctx.send(text_translated)
 
 
 token  = configData["token"]
