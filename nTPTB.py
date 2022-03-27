2e886dceaf7390798f415474b27f459467006f3e
shorewind <99942150+shorewind@users.noreply.github.com>
2022-03-26 19:23:05 -0500

    Add files via upload

--- nTPTB.py
+++ nTPTB.py
@@ -1,8 +1,10 @@
 from discord.ext import commands
 import json
 import os
+
 from lib2to3.pgen2.tokenize import tokenize
 import translators as translate
+
 
 if os.path.exists(os.getcwd() + "/config.json"):
     with open(".\config.json") as f:
@@ -31,26 +33,31 @@ class translator(commands.Cog):
     async def echo(self, ctx, *, message):
         return await ctx.send(message)
    
-    @commands.command(name='translate', aliases = ['tl', 'Tl', 'tL', 'TL'])
+    @commands.command(name='translate', aliases = ['tl', 'tr'])
     async def translate(self, ctx, from_language, to_language, *, message):
         translated_message = translate.google(message, str(from_language), str(to_language))
         return await ctx.send(translated_message)
 
-    @commands.command(name = 'TranslateTTs', aliases = ['tltts'])
+    @commands.command(name = 'TranslateTTs', aliases = ['tltts', 'translatetts', 'translateTTS', 'translateTTs'])
     async def translateTTS(self,ctx, from_language, to_language, *, message):
          translated_message = translate.google(message, str(from_language), str(to_language))
          return await ctx.send(translated_message, tts=True)
 
-    @commands.command(name = 'countryGuide', aliases = ['cg', 'cG', 'Cg'])
+    @commands.command(name = 'languageGuide', aliases = ['lg', 'languageguide','languages'])
     async def countryGuide(self, ctx):
-        file = open("countries.txt", 'r')
+        file = open("languages.txt", 'r')
         await ctx.send(file.read())
         file.close()
 
-    @commands.command(name = 'help', aliases = ['h', '?'])
+    @commands.command(name = 'help', aliases = ['h', '?', 'Help', 'HELP', 'guide', 'commands'])
     async def helper(self, ctx, to_language='en'):
-        help_message = 'help message'
+        help_title = '**Command Guide**\n'
+        help_translate = '__ Translate __\n$tl|tr [from language] [to language] [message]\n'
+        help_translatetts = '__ Text-to-Speech __\n$tltts [from language] [to language] [message]\n'
+        help_languageguide = '__ Language Guide __\n$lg '
+        help_message = help_title + help_translate + help_translatetts + help_languageguide
         translated_help_message = translate.google(help_message, 'en', str(to_language))
+        translated_help_message = translated_help_message.lower()
         return await ctx.send(translated_help_message)
 
 
